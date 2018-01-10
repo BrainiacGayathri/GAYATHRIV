dcf2861f1d167af392b37f1328977e6ab19fad37
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-01-10 16:24:22 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,23 @@
+  #include<stdio.h>
+ int main()
+ {
+ 	int a=3;
+ 	if(a%400==0)
+ 	{
+ 	printf("LEAP YR"); 
+ 	}
+ 	else if(a%4==0)
+ 	{
+ 	printf("leap yr");
+ 	}
+  else if("a%100==0)
+  {
+  printf("leap yr");
+  }
+  else
+  {
+  printf("not leap yr");
+  }
+ 	return 0;
+ 	getch();
+ }
