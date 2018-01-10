29fd1b7830a2d043d0fdd855a0f6388d6f017f84
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-01-10 16:08:38 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf("enter char");
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+{
+printf("vowel");
+}
+else
+{
+printf("consonant");
+}
+getch();
+}
