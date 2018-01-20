b69686e47e0195de7d14e8b0d0067b6aca83126d
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-01-20 18:24:31 +0530

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+long int n;
+scanf("%ld\n",&n);
+long int a[n];
+long int i,min;
+
+for(i=0;i<n;i++)
+{
+ scanf("%ld ",&a[i]);
+}
+min=a[0];
+
+for(i=1;i<n;i++)
+{ if(min>a[i])
+   min=a[i];
+}
+printf("%ld",min);
+getch();
+}
+
+  
+  
+
