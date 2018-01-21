7282e6b3d6c34bb69aef1e134e7600dd70626765
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-01-21 05:34:42 +0000

    Create 37.c

---
+++ 37.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int m,n,temp;
+	printf("enter the two number");
+	scanf("%d %d",&n,&m);
+	printf("before swap%d %d\n",n,m);
+	
+  temp=n;
+  n=m;
+  m=temp;
+  printf("after swap%d %d",n,m);
+  getch();
+}
