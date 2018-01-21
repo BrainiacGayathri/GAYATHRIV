0b7ea767006981339073e18aa9dbbc026ce14dc8
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-01-21 04:00:29 +0000

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n,i,a[10];
+printf("enter the number");
+scanf("%d",&n);
+printf("array elements");
+for(i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+}
+printf("elements with index ");
+for(i=0;i<n;i++)
+{
+  if(i<n)
+  {
+    printf("%d %d\n",a[i],i );
+    }
+    }
+    getch();
+}
