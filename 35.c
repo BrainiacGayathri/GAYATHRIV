e90d94eaeed26048462450a534be57354ac59463
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-01-21 04:40:13 +0000

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.>
+void main()
+{
+char a[50];
+int i,cnt=0;
+printf("Enter the senet");
+scanf("%s",&a);
+for(i=0;a[i]!=null;i++)
+{
+if(a[i]==1-9)
+{
+cnt++;
+}
+}
+printf("%d",cnt);
+getch();
+}
