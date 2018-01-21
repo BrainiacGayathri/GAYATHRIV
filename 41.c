2cbdd528101b7a50c0206c1a2d2534265e82503c
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-01-21 04:42:59 +0000

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int i,b;
+    char a[i];
+    scanf("%s",&b);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    printf("%S",a);
+    }
+    getch();
+}
+ 
