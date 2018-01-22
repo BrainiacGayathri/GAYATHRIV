f76d5d5210bd6a3b6c554f85482c35618c24633d
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-01-22 18:07:21 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c,d,e,f;
+	printf("enter the numbers:");
+	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+	f=(a+b+c+d+e)/5;
+	printf("the number is %d",f);
+	getch();
+}
