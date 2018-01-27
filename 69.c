ab71a6b780e3b76f4165e26c26f5ce0781e1f349
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-01-27 17:42:01 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h?
+void main()
+{
+int a,n;
+scanf("%d%d",&a,&n);
+printf("/n%d %d",a,n);
+int c=a-n;
+printf("\n the difference is %d",c);
+if(c%2==0)
+{
+printf("\neven");
+
+}
+else
+{
+printf("\nodd");
+}
+getch();
+}
