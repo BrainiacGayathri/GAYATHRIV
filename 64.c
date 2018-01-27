d269f0fe37e0da08d209a4cecf829128785047d3
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-01-27 17:36:40 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+void main
+{
+	int a,b,d;
+	printf("\nEnter the two numbers: ");
+	scanf("%d",&a);
+	scanf("%d",&b);
+		n=a+b;
+	if(d%2==0)
+	{
+		printf("\nThe sum is even.");
+	}
+	else
+	{
+		printf("\nThe sum is odd. ");
+	}
+	getch();
+}
