27f30668779d82ced3b612254a9716353f624179
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-02-03 17:14:28 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+  int b,count=0,i,n;
+  printf("enter the number");
+  scanf("%d",&b);
+  for(i=1;i<=b;i++)
+  {
+    if(n%i==0)
+    
+{
+  count++;
+}
+  }
+  if(count==2)
+  {
+    printf("no");
+  }
+  else
+  {
+    printf("yes");
+  }
+getch();
+
+}
