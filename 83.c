5062f8b1b746b627b8bfec8fc72606bc25ef692c
BrainiacGayathri <32446386+BrainiacGayathri@users.noreply.github.com>
2018-02-12 15:51:18 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void manin()
+{
+long int a,b,div,mod,n;
+scanf("%d",&n);
+switch(n)
+{
+	case 1:
+	scanf("%ld / %ld",&a,&b);
+	printf("%ld",a/b);
+	break;
+	case 2:
+	scanf("%ld % %ld",&a,&b);
+
+	printf("%ld",(a%b));
+
+	break;
+  getch();
+}
